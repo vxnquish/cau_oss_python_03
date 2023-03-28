118bc8bbb8c22ae3dea138f691273b52c6210a4a
vxnquish <128028726+vxnquish@users.noreply.github.com>
2023-03-28 10:07:28 +0900

    updated_volume_calc.py
    부피 계산하는 툴 + 박스의 총 길이에 따른 택배 요금을 구해주는 툴

--- volume_calc.py
+++ volume_calc.py
@@ -1,7 +1,42 @@
+#%% 부피 계산
+
 length = float(input("length:"))
 width = float(input("width:"))
 height = float(input("height:"))
 
 volume = length*width*height
 
-print("부피는",volume)
+print("부피는",volume, "입니다.")
+
+#
+#
+#
+
+# %% 택배 요금 계산
+
+# 80cm 이하 : 5000
+# 80 ~ 100cm : 8000
+# 100 ~ 120cm : 10000
+# 120 ~ 160cm : 13000
+# 160cm 초과 : 배송 불가
+
+length = float(input("length:"))
+width = float(input("width:"))
+height = float(input("height:"))
+
+total_length = (length + width + height)
+
+
+if total_length <= 80:
+    charge = 5000
+elif total_length <= 100:
+    charge = 8000
+elif total_length <= 120:
+    charge = 10000
+elif total_length <= 160:
+    charge = 13000
+else:
+    charge = "배송 불가"
+
+print("총 길이는", total_length, "입니다.")
+print("요금은", charge, "입니다.")
